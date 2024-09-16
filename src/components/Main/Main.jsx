83e24a1bc87e90a4200b610ca3b762659115b2cd
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-09-15 21:11:31 -0400

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main>
+      <img src="" alt="" />
+      <div>Gryffindor</div>
+      <div>Slytherin</div>
+      <div>Ravenclaw</div>
+      <div>Hufflepuff</div>
+    </main>
+  );
+}
